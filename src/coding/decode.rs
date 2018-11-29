bf423dcda170b565f82d26ca62fea2090b5b9994
Michiel Visser <info@webmichiel.nl>
2018-11-29 22:42:44 +0100

    Add comments to encoder and decoder

--- src/coding/decode.rs
+++ src/coding/decode.rs
@@ -11,8 +11,10 @@ pub struct ACDecoder<I> {
 }
 
 impl<I> ACDecoder<I> where I: Iterator<Item = bool> {
+    // Create a new arithmetic decoder
     pub fn new(iter: I) -> Self {
-        let probs = vec![1; 257];
+        // Start off with equal probabilities for each symbol
+        let probs = vec![1; SYMBOLS];
 
         let mut decoder = Self {
             iter: iter,
@@ -22,11 +24,13 @@ impl<I> ACDecoder<I> where I: Iterator<Item = bool> {
             range: Range::new(),
             fraction: 0,
         };
+        // Set-up the initial values of the fraction
         decoder.initial_fraction();
         decoder
     }
 
     fn initial_fraction(&mut self) {
+        // Load the initial value for the fraction from the bitstream
         for i in 0..PRECISION {
             if let Some(true) = self.iter.next() {
                 self.fraction += 1 << (PRECISION - (i + 1));
@@ -35,6 +39,7 @@ impl<I> ACDecoder<I> where I: Iterator<Item = bool> {
     }
 
     fn increment_fraction(&mut self) {
+        // Update the fraction with a new bit
         if let Some(true) = self.iter.next() {
             self.fraction += 1;
         }
@@ -45,27 +50,37 @@ impl<I> Iterator for ACDecoder<I> where I: Iterator<Item = bool> {
     type Item = u8;
 
     fn next(&mut self) -> Option<Self::Item> {
+        // When finished always return None
         if self.finished {
             return None;
         }
 
         let mut result: Option<u8> = None;
+        // Try to find a symbol which matches the fraction
         for symbol in 0..SYMBOLS {
+            // Generate the range for the possible symbol
             let new_range = self.range.select_symbol(symbol, &self.probability);
 
+            // Check that this new range contains the fraction
             if new_range.contains(self.fraction) {
+                // If the encoded symbol is an EOF finish decoding
                 if symbol == EOF {
                     self.finished = true;
                     return None;
                 }
 
+                // Update the current range
                 self.range = new_range;
+                // Increment the probability for this symbol, to stay in sync
+                // with the encoder
                 self.probability.increment(symbol);
+
                 result = Some(symbol as u8);
                 break;
             }
         }
 
+        // Rescale the current range when in the bottom or top half
         while self.range.in_bottom_half() || self.range.in_upper_half() {
             if self.range.in_bottom_half() {
                 self.range.scale_bottom_half();
@@ -78,12 +93,14 @@ impl<I> Iterator for ACDecoder<I> where I: Iterator<Item = bool> {
             self.increment_fraction();
         }
 
+        // Rescale the range when in the middle half
         while self.range.in_middle_half() {
             self.range.scale_middle_half();
             self.fraction = 2 * (self.fraction - QUARTER);
             self.increment_fraction();
         }
 
+        // Return the decoded symbol
         result
     }
 }
--- src/coding/encode.rs
+++ src/coding/encode.rs
@@ -13,7 +13,9 @@ pub struct ACEncoder<I> {
 }
 
 impl<I> ACEncoder<I> where I: Iterator<Item = u8> {
+    // Create a new arithmetic encoder
     pub fn new(iter: I) -> Self {
+        // Start off with equal probabilities for each symbol
         let probs = vec![1; SYMBOLS];
 
         Self {
@@ -29,6 +31,8 @@ impl<I> ACEncoder<I> where I: Iterator<Item = u8> {
     }
 
     fn emit(&mut self, bit: bool) {
+        // Emit a number of bits, this is always a bit, followed by a number of
+        // opposite bits, depending on the times we were in the middle half
         self.emit_buf.push(bit);
         for _ in 0..self.middle_count {
             self.emit_buf.push(!bit);
@@ -37,33 +41,43 @@ impl<I> ACEncoder<I> where I: Iterator<Item = u8> {
     }
 
     fn generate_for_symbol(&mut self, symbol: usize) {
+        // Change the range to the sub-range for this symbol
         self.range = self.range.select_symbol(symbol, &self.probability);
 
+        // When in the bottom or top half we encode a zero or one
         while self.range.in_bottom_half() || self.range.in_upper_half() {
             if self.range.in_bottom_half() {
+                // Rescale to the bottom half
                 self.range.scale_bottom_half();
+                // Emit a zero
                 self.emit(false);
             } else if self.range.in_upper_half() {
+                // Rescale to the upper half
                 self.range.scale_upper_half();
+                // Emit a one
                 self.emit(true);
             }
         }
 
+        // When in the middle half, rescale to the middle half and count
         while self.range.in_middle_half() {
             self.middle_count += 1;
             self.range.scale_middle_half();
         }
 
+        // Increment the probability for this symbol
         self.probability.increment(symbol);
     }
 
     fn finalize(&mut self) {
+        // Finialize the encoding of the last symbol
         self.middle_count += 1;
         if self.range.in_bottom_quarter() {
             self.emit(false);
         } else {
             self.emit(true);
         }
+        // Set finished
         self.finished = true;
     }
 }
@@ -72,19 +86,24 @@ impl<I> Iterator for ACEncoder<I> where I: Iterator<Item = u8> {
     type Item = bool;
 
     fn next(&mut self) -> Option<Self::Item> {
+        // As long as there are no bits to output, generate some more bits
         while self.emit_buf.is_empty() {
+            // If we are finished return None
             if self.finished {
                 return None;
             }
 
             if let Some(symbol) = self.iter.next() {
+                // If there is a symbol left, generate bits for the symbol
                 self.generate_for_symbol(symbol as usize);
             } else {
+                // Otherwise generate an EOF and finalize the bits
                 self.generate_for_symbol(EOF);
                 self.finalize();
             }
         }
 
+        // Return the first available bit
         Some(self.emit_buf.remove(0))
     }
 }
